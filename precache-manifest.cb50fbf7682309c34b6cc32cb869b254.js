2d0b3d8fb797c2ed03d142dc793e10a59ec0b65d
Code Sandbox <hello@codesandbox.io>
2023-02-09 20:14:43 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.cb50fbf7682309c34b6cc32cb869b254.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36fc1dad72d938c446d6df2d377fb570",
+    "url": "https://roshdezign.github.io/csb-hhqr9z/index.html"
+  },
+  {
+    "revision": "d7ba5c4036f91ed567dd",
+    "url": "https://roshdezign.github.io/csb-hhqr9z/static/css/main.a79bd594.chunk.css"
+  },
+  {
+    "revision": "8268a0bb945486ecef3e",
+    "url": "https://roshdezign.github.io/csb-hhqr9z/static/js/2.29a17a2c.chunk.js"
+  },
+  {
+    "revision": "d7ba5c4036f91ed567dd",
+    "url": "https://roshdezign.github.io/csb-hhqr9z/static/js/main.8bc32a57.chunk.js"
+  },
+  {
+    "revision": "e49f791131322b3b807d",
+    "url": "https://roshdezign.github.io/csb-hhqr9z/static/js/runtime~main.75ab0725.js"
+  }
+]);
